469958c9f267bb3b69f300e114d84c83ed6a07c6
zhaiyuhang <zhaiyuhang@zhaiyuhangdeMacBook-Pro.local>
2014-11-13 19:56:26 +0800

    优化逻辑

--- once.go
+++ once.go
@@ -1,4 +1,4 @@
-package goredis
+package msgRedis
 
 import (
 	"errors"
@@ -8,19 +8,15 @@ type OnceConn struct {
 	*Conn
 }
 
-func NewOnceConn() *OnceConn {
-	return &OnceConn{}
-}
-
 func (mp *MultiPool) CallOnce(address string) *OnceConn {
-	oc := NewOnceConn()
+	oc := &OnceConn{}
 	c := mp.PopByAddr(address)
 	if c == nil {
-		oc.Conn = NewConn(nil, 0, 0, 0, false, nil, "")
-		oc.Conn.err = errors.New("get a nil conn address=" + address)
+		oc.Conn = &Conn{err: errors.New("get a nil conn address=" + address)}
 		return oc
 	}
 	oc.Conn = c
 	oc.Conn.isOnce = true
 	return oc
+
 }
